9c34142504010a70c417e35ec1576c7a95ae5140
hoangdaidp <65940270+hoangdaidp@users.noreply.github.com>
2021-03-11 11:24:06 +0700

    set up basic source

---
+++ TMDT/src/main/java/com/website/TMDT/Controller/TestController.java
@@ -0,0 +1,13 @@
+package com.website.TMDT.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+	}
+}
