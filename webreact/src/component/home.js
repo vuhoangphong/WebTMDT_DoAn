9d1a9fc004618c926c20b34c0354f8bc8b1116ab
hoangdaidp <65940270+hoangdaidp@users.noreply.github.com>
2021-03-11 13:36:01 +0700

    add product test files

---
+++ webreact/src/component/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function home() {
+  return (
+    <div className="test">
+        <h1>home</h1>
+    </div>
+  );
+}
+
+export default home;
---
+++ webreact/src/component/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function test() {
+  return (
+    <div className="test">
+        <h1>test</h1>
+    </div>
+  );
+}
+
+export default test;
