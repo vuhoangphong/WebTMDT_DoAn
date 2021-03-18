6770d9966f396be268b61f0958b5c3666e31b917
CaoDacLuan <76766255+caodacluan@users.noreply.github.com>
2021-03-18 23:23:54 +0700

    update register login shop

--- webreact/src/component/shop/LoginShop/loginshop.jsx
+++ webreact/src/component/shop/LoginShop/loginshop.jsx
@@ -11,21 +11,25 @@ function loginshop() {
                     </div>
 
                     <div class="container">
-                        <label for="uname"><b>Username</b></label>
+                        <label for="uname"><b>Username:</b></label>
                         <input type="text" placeholder="Enter Username" name="uname" required/>
                         <br/>
-                        <label for="psw"><b>Password</b></label>
+                        <label for="psw"><b>Password:</b></label>
                         <input type="password" placeholder="Enter Password" name="psw" required/>
                         <br/>
-                        <button type="submit">Login</button>
                         <label>
                         <input type="checkbox" checked="checked" name="remember" required/> Remember me
                         </label>
+                        <br/>
+                        <div class="btn-signin">
+                           <a href="./shop">Sign in</a>
+                        </div>
+                        <br/>
+                        <span class="psw">Forgot <a href="#">password ?</a></span>
                     </div>  
-                    
+                    <br/>
                     <div class="container" >
                         <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-                        <span class="psw">Forgot <a href="#">password?</a></span>
                     </div>
                 </form>
             </div>
--- webreact/src/component/shop/RegisterShop/registershop.jsx
+++ webreact/src/component/shop/RegisterShop/registershop.jsx
@@ -9,58 +9,64 @@ function registershop() {
             <p>Vui Lòng Điền Đầy Đủ Thông Tin Cửa Hàng</p>
             <form action="">
                 <hr/> 
-                    <label for="tenshop"><b>Tên Shop</b></label>
-                    <input type="text" placeholder="Nhập Tên Shop" name="ts" id="ts" required/>
-                    <br/>
-                    <label for="tendangnhap"><b>Tên Đăng Nhập</b></label>
-                    <input type="text" placeholder="Nhập Tên Đăng Nhập" name="tdn" id="tdn" required/>
-                    <br/>
-                    <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Nhập Password" name="psw" id="psw" required/>
-                    <br/>
-                    <label for="psw-repeat"><b>Repeat Password</b></label>
-                    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required/>
-                    <br/>
-                    <label for="email"><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="email" id="email" required/>
-                    <br/>
-                    <label for="diachi"><b>Địa Chỉ</b></label>
-                    <input type="text" placeholder="Nhập Địa Chỉ" name="dc" id="dc" required/>
-                    <br/>
-                    <label for="quan"><b>Quận</b></label>
-                    <input type="text" placeholder="Nhập Quận" name="q" id="q" required/>
-                    <br/>
-                    <label for="phuong"><b>Phường</b></label>
-                    <input type="text" placeholder="Nhập Phường" name="p" id="p" required/>
-                    <br/>
-                    <div class="custom-select">
-                        <label for="tinhthanh"><b>Tỉnh/Thành</b></label>
-                        <select>
-                            <option value="0"></option>
-                            <option value="1">An Giang</option>
-                            <option value="2">Bạc Liêu</option>
-                            <option value="3">Bình Dương</option>
-                            <option value="4">Cao Bằng</option>
-                            <option value="5">Đắk Lắk</option>
-                            <option value="6">Đồng Nai</option>
-                            <option value="7">Khánh Hòa</option>
-                            <option value="8">Long An</option>
-                            <option value="9">Phú Thọ</option>
-                            <option value="10">Sóc Trăng</option>
-                            <option value="11">Thanh Hóa</option>
-                            <option value="12">Trà Vinh</option>
-                            <option value="13">Cần Thơ</option>
-                            <option value="14">Đà Nẵng</option>
-                            <option value="15">Hải Phòng</option>
-                            <option value="16">Hà Nội</option>
-                            <option value="17">TP HCM</option>     
-                        </select>
-                    </div>
-                    <br/>
+                    <table width="50">
+                        <label for="tenshop"><b>Tên Shop</b></label>
+                        <input type="text" placeholder="Nhập Tên Shop" name="nameshop" id="nameshop" required/>
+                        <br/>
+                        <label for="tendangnhap"><b>Tên Đăng Nhập</b></label>
+                        <input type="text" placeholder="Nhập Tên Đăng Nhập" name="user" id="user" required/>
+                        <br/>
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Nhập Password" name="password" id="password" required/>
+                        <br/>
+                        <label for="psw-repeat"><b>Repeat Password</b></label>
+                        <input type="password" placeholder="Repeat Password" name="password-repeat" id="password-repeat" required/>
+                        <br/>
+                        <label for="email"><b>Email</b></label>
+                        <input type="text" placeholder="Enter Email" name="email" id="email" required/>
+                        <br/>
+                        <label for="diachi"><b>Địa Chỉ</b></label>
+                        <input type="text" placeholder="Nhập Địa Chỉ" name="address" id="address" required/>
+                        <br/>
+                        <label for="quan"><b>Quận</b></label>
+                        <input type="text" placeholder="Nhập Quận" name="district" id="district" required/>
+                        <br/>
+                        <label for="phuong"><b>Phường</b></label>
+                        <input type="text" placeholder="Nhập Phường" name="ward" id="ward" required/>
+                        <br/>
+                        <div class="custom-select">
+                            <label for="tinhthanh"><b>Tỉnh/Thành</b></label>
+                            <select>
+                                <option value="0"></option>
+                                <option value="1">An Giang</option>
+                                <option value="2">Bạc Liêu</option>
+                                <option value="3">Bình Dương</option>
+                                <option value="4">Cao Bằng</option>
+                                <option value="5">Đắk Lắk</option>
+                                <option value="6">Đồng Nai</option>
+                                <option value="7">Khánh Hòa</option>
+                                <option value="8">Long An</option>
+                                <option value="9">Phú Thọ</option>
+                                <option value="10">Sóc Trăng</option>
+                                <option value="11">Thanh Hóa</option>
+                                <option value="12">Trà Vinh</option>
+                                <option value="13">Cần Thơ</option>
+                                <option value="14">Đà Nẵng</option>
+                                <option value="15">Hải Phòng</option>
+                                <option value="16">Hà Nội</option>
+                                <option value="17">TP HCM</option>     
+                            </select>
+                        </div>
+                    </table>
                 <hr/>
-                <button type="submit" class="registerbtn" value="register">Đăng Ký</button>
+                <div class="btn-register" >
+                    <a href="./loginshop">Register</a>
+                </div>
+                <br/>
+                <br/>
                 <div class="container signin">
-                    <p>Already have an account? <a href="#">Sign in</a>.</p>
+                    <p>Already have an account? <a href="./loginshop">Sign in</a>.</p>
+                    
                 </div>
             </form> 
             </div> 
